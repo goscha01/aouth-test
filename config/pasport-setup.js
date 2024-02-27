db30113a2e48caf83cbc3011b31656b8225cd132
George Sayapin <prorabserv@gmail.com>
2024-02-27 15:18:03 -0500

    added user to DB

--- config/pasport-setup.js
+++ config/pasport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys')
+const User = require('../models/user-model');
 
 
 passport.use(new GoogleStrategy({
@@ -9,8 +10,20 @@ passport.use(new GoogleStrategy({
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
 }, (accesToken, refreshToken, profile, done) => {
-    // PASSPORT CALLBACK FUNSTION
-    console.log('passport callback');
-    console.log(profile);
+    // check if user allready exists in DB
+    User.findOne({googleId: profile.id}).then((currentUser) => {
+        if(currentUser){
+            console.log('esixting user : ' + currentUser);
+        } else {
+            new User({
+                username: profile.displayName,
+                googleId: profile.id
+            }).save().then((newUser) => {
+                console.log('new user created: ' + newUser);
+            })
+        }
+    })
+
+    
 })
 );
--- models/user-model.js
+++ models/user-model.js
@@ -6,6 +6,6 @@ const userSchema = new Schema({
     googleId: String
 });
 
-const User = mongoose.model('user', userShema);
+const User = mongoose.model('user', userSchema);
 
 module.exports = User;
